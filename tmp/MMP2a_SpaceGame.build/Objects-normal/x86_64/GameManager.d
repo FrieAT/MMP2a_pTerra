b4814398e1c617df24c6f1ef9e963c96c7c18322
Andreas Friedle <afriedle.mmt-b2014@fh-salzburg.ac.at>
2015-11-16 17:48:53 +0100

    cleanup

--- tmp/MMP2a_SpaceGame.build/Objects-normal/x86_64/GameManager.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/LaVey/XCodeProjects/MMP2a_SpaceGame/source/GameManager.cpp \
-  /Users/LaVey/XCodeProjects/MMP2a_SpaceGame/include/GameManager.h \
-  /Users/LaVey/XCodeProjects/MMP2a_SpaceGame/include/IManager.h
