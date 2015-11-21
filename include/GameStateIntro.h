ccc9a9da048665721548acb8a1bc420a7c0eeda3
Friedrich Schmidt <fschmidt.mmt-b2014@fh-salzburg.ac.at>
2015-11-21 10:18:30 +0100

    Implemented State-Change on pressing FIRE-Key

--- include/GameStateIntro.h
+++ include/GameStateIntro.h
@@ -2,15 +2,18 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "IInputObserver.h"
 #include "IGameState.h"
 
-class GameStateIntro : public IGameState
+class GameStateIntro : public IGameState, public IInputObserver
 {
 public:
     ~GameStateIntro();
     void Init();
     void Update(sf::RenderWindow* window);
+    void OnInputUpdate(std::string event);
 protected:
     sf::Texture* texture;
     sf::Sprite* sprite;
+    bool KeyPressed;
 };
--- source/GameStateIntro.cpp
+++ source/GameStateIntro.cpp
@@ -1,15 +1,22 @@
 #include "GameStateIntro.h"
 
+#include "Game.h"
 #include "InputManager.h"
 
 GameStateIntro::~GameStateIntro()
 {
+    InputManager::UnregisterEventObserver(this);
+    
     delete texture;
     delete sprite;
 }
 
 void GameStateIntro::Init()
 {
+    KeyPressed = false;
+    
+    InputManager::RegisterEventObserver(this);
+    
     // ====== Below decprecated method to create things ======
     
     // Load a sprite to display
@@ -25,4 +32,19 @@ void GameStateIntro::Update(sf::RenderWindow* window)
 {
     // Draw the sprite
     window->draw(*sprite);
+}
+
+void GameStateIntro::OnInputUpdate(std::string event)
+{
+    std::size_t delimiter_pos = event.find('_');
+    if(delimiter_pos == std::string::npos) return;
+    std::string key = event.substr(0, delimiter_pos);
+    std::string state = event.substr(delimiter_pos+1, 1);
+    
+    if(state == "P" && key == "FIRE" && !KeyPressed)
+    {
+        KeyPressed = true;
+        Game::Engine->ChangeState(new GameStateIntro());
+        Game::Engine->Start();
+    }
 }
