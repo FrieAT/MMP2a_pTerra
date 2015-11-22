449ce6421e31ab486101d123b475a54e9b2197cd
Friedrich Schmidt <fschmidt.mmt-b2014@fh-salzburg.ac.at>
2015-11-22 17:40:28 +0100

    Added possibility for CreateBackgroundSprite to add manually a path as parameter

--- source/GameObjectFactory.cpp
+++ source/GameObjectFactory.cpp
@@ -18,13 +18,13 @@ GameObject* GameObjectFactory::CreatePlayerShip()
 	return ship;
 }
 
-GameObject* GameObjectFactory::CreateBackgroundSprite()
+GameObject* GameObjectFactory::CreateBackgroundSprite(std::string background_path)
 {
     // TODO: Make it possible to change component values / change the whole factory
     GameObject* background = new GameObject("background");
     
     background->SetComponent(new PixelPosition(sf::Vector2f(0.f, 0.f), sf::Vector2f(0.f, 0.f)));
-    background->SetComponent(new SpriteDrawing("assets/space-map.jpg"));
+    background->SetComponent(new SpriteDrawing(background_path));
     // background->SetComponent(new ScriptedView(sf::FloatRect(0, 0, 800, 600), sf::Vector2f(1120, 0), 0.025f));
     
     return background;
