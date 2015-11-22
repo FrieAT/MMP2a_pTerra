d3c0fec7382b4a0dc7d214f17a9d4cb79a7feb90
Friedrich Schmidt <fschmidt.mmt-b2014@fh-salzburg.ac.at>
2015-11-22 09:15:51 +0100

    ScriptedView Implementation

--- include/ScriptedView.h
+++ include/ScriptedView.h
@@ -5,10 +5,15 @@
 class ScriptedView : public IView
 {
 public:
-    ScriptedView(sf::Vector2f start_pos, sf::Vector2f end_pos, float duration);
+    ScriptedView(sf::FloatRect fViewSize, sf::Vector2f fMoveVector, float fSpeed);
     ~ScriptedView();
     void Update();
     void Draw(sf::RenderWindow* window);
 private:
-    
+    sf::Vector2f m_MoveVector;
+    float m_fSteps;
+    float m_fSpeed;
+    sf::View* m_pView;
+    sf::Clock clock;
+    void Initialize();
 };
--- source/ScriptedView.cpp
+++ source/ScriptedView.cpp
@@ -1,21 +1,31 @@
+#include <math.h>
+
 #include "ScriptedView.h"
 
-ScriptedView::ScriptedView(sf::Vector2f start_pos, sf::Vector2f end_pos, float duration)
+ScriptedView::ScriptedView(sf::FloatRect fViewSize, sf::Vector2f fMoveVector, float fSpeed)
 {
+    float fStep = (fSpeed / sqrt(pow(fMoveVector.x,2.0) + pow(fMoveVector.y,2.0)));
     
+    m_MoveVector = fMoveVector;
+    m_fSteps = fStep;
+    m_fSpeed = fSpeed;
+    
+    m_pView = new sf::View(fViewSize);
 }
 
 ScriptedView::~ScriptedView()
 {
-    
+    delete m_pView;
 }
 
 void ScriptedView::Update()
 {
-    
+    sf::Time delta_time = clock.restart();
+    float fStepsWithDeltaTime = m_fSteps * delta_time.asMilliseconds();
+    m_pView->move(m_MoveVector * fStepsWithDeltaTime);
 }
 
 void ScriptedView::Draw(sf::RenderWindow *window)
 {
-    
+    window->setView(*m_pView);
 }
