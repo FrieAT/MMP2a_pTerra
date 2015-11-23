65e3f952e5d4b195a54a010e5d06fd5e7ca1b54a
Friedrich Schmidt <fschmidt.mmt-b2014@fh-salzburg.ac.at>
2015-11-23 11:18:53 +0100

    Removed mistake messing with GameObject-constructor

--- source/GameObjectFactory.cpp
+++ source/GameObjectFactory.cpp
@@ -11,7 +11,7 @@
 GameObject* GameObjectFactory::CreatePlayerShip()
 {
 	// TODO: Make it possible to change component values / change the whole factory
-	GameObject* ship = new GameObject(std::string("ship"));
+	GameObject* ship = new GameObject("ship");
 
 	ship->SetComponent(new PixelPosition(sf::Vector2f(40.f, 40.f), sf::Vector2f(32.f, 32.f)));
 	ship->SetComponent(new ShipMovement());
@@ -23,7 +23,7 @@ GameObject* GameObjectFactory::CreatePlayerShip()
 GameObject* GameObjectFactory::CreateBackgroundSprite(std::string background_path)
 {
     // TODO: Make it possible to change component values / change the whole factory
-    GameObject* background = new GameObject(std::string("background"));
+    GameObject* background = new GameObject("background");
     
     background->SetComponent(new PixelPosition(sf::Vector2f(0.f, 0.f), sf::Vector2f(0.f, 0.f)));
     background->SetComponent(new SpriteDrawing(background_path));
@@ -33,7 +33,7 @@ GameObject* GameObjectFactory::CreateBackgroundSprite(std::string background_pat
 
 GameObject* GameObjectFactory::CreateScriptedView()
 {
-    GameObject* view = new GameObject(std::string("background"));
+    GameObject* view = new GameObject("background");
     
     view->SetComponent(new ScriptedView(sf::FloatRect(0, 0, static_cast<float>(Game::m_iWindowWidth), static_cast<float>(Game::m_iWindowHeight)), sf::Vector2f(1920.f - static_cast<float>(Game::m_iWindowWidth), 0), 20.f));
     
@@ -42,7 +42,7 @@ GameObject* GameObjectFactory::CreateScriptedView()
 
 GameObject* GameObjectFactory::CreateFontText(sf::Vector2f Position, std::string sFontPath, std::string sText, int iCharSize)
 {
-    GameObject* font_text = new GameObject(std::string(std::string("text")));
+    GameObject* font_text = new GameObject("text");
     
     font_text->SetComponent(new PixelPosition(Position, sf::Vector2f(0.f, 0.f)));
     font_text->SetComponent(new FontDrawing(sFontPath, sText, iCharSize));
