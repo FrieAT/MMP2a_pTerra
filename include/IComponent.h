f46a531c1e61b6d0e8e3d81a60934c1e7976b832
Friedrich Schmidt <fschmidt.mmt-b2014@fh-salzburg.ac.at>
2015-12-23 13:16:32 +0100

    Added optional Init()-Method for Components, which is called after the GameObject is assigned to the Component

--- include/IComponent.h
+++ include/IComponent.h
@@ -14,6 +14,7 @@ class IComponent
 public:
     IComponent() : m_pGameObject(nullptr) { }
     virtual ~IComponent() { }
+    virtual void Init() { }
 	virtual EComponentType GetComponentType() = 0;
 	void SetAssignedGameObject(GameObject* pObject)
 	{
