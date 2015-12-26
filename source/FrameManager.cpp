060e003c6cf8a2ee0490de2130f064f58bd8a7e8
Friedrich Schmidt <fschmidt.mmt-b2014@fh-salzburg.ac.at>
2015-12-26 16:41:26 +0100

    Skipping FrameManager-Update´s on Objects, which are in a Quadrant that is in a Freezed-State

--- source/FrameManager.cpp
+++ source/FrameManager.cpp
@@ -3,13 +3,38 @@ Copyright (c) MultiMediaTechnology, 2015
 =================================================================*/
 
 #include "FrameManager.h"
+#include "IComponent.h"
+#include "IPosition.h"
 
 void FrameManager::Update(sf::Time DeltaTime)
 {
+    // int count = 0;
     for (unsigned int i = 0; i < m_Observers.size(); i++)
     {
+        IComponent* pComponent = dynamic_cast<IComponent*>(m_Observers[i]);
+        // Normally a Observer of FrameManager is always a Component, but who knows the future.
+        if(pComponent != nullptr)
+        {
+            IPosition* pPositionComponent = static_cast<IPosition*>(pComponent->GetAssignedGameObject()->GetComponent(EComponentType::Position));
+            // If component has a IPosition (which is maybe always)
+            if(pPositionComponent != nullptr)
+            {
+                // Check if current Quadrant is freezed or not.
+                // if so, don´t perform OnFrameUpdate on Observer.
+                Quadrant* pQuadrant = pPositionComponent->GetQuadrant();
+                if(pQuadrant != nullptr && pQuadrant->GetFreezedState())
+                {
+                    // Skip observer call for this GameObject.
+                    continue;
+                }
+            }
+        }
+        
         m_Observers[i]->OnFrameUpdate(DeltaTime);
+        // count++;
     }
+    
+    // std::cout << "Frame Manager Update called for " << count << " Objects." << std::endl;
 }
 
 void FrameManager::Draw(sf::RenderWindow* pWindow)
