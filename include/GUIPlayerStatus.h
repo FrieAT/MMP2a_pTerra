a1f9cc41fb29fbb06bd73f94023197bdaca5e271
Andreas Friedle <afriedle.mmt-b2014@fh-salzburg.ac.at>
2016-01-23 18:44:12 +0100

    Added EventBus and started implementation

---
+++ include/GUIPlayerStatus.h
@@ -0,0 +1,15 @@
+/*=================================================================
+Copyright (c) MultiMediaTechnology, 2015
+=================================================================*/
+
+#pragma once
+
+#include "eventbus\EventHandler.hpp"
+
+class GUIPlayerStatus
+{
+public:
+	GUIPlayerStatus();
+	~GUIPlayerStatus();
+};
+
---
+++ source/GUIPlayerStatus.cpp
@@ -0,0 +1,18 @@
+/*=================================================================
+Copyright (c) MultiMediaTechnology, 2015
+=================================================================*/
+
+#include "stdafx.h"
+
+#include "..\include\GUIPlayerStatus.h"
+
+
+
+GUIPlayerStatus::GUIPlayerStatus()
+{
+}
+
+
+GUIPlayerStatus::~GUIPlayerStatus()
+{
+}
